9b68b6fdbc2bc73df419002d951c434dc474f00e
Marcelo <mmandolesi@gannett.com>
2020-12-29 19:24:23 -0500

    Day 12 part 1 cleanup

--- day-12/day-12.go
+++ day-12/day-12.go
@@ -9,6 +9,65 @@ import (
 )
 
 func main() {
+	directions := setup()
+
+	bearing := 90  // Start facing east (090°).
+	positionX := 0 // East, West
+	positionY := 0 // North, South
+	for _, node := range directions {
+		switch node.direction {
+		case "S":
+			positionY = positionY - node.amount
+		case "N":
+			positionY = positionY + node.amount
+		case "E":
+			positionX = positionX + node.amount
+		case "W":
+			positionX = positionX - node.amount
+		case "F":
+			switch bearing {
+			case 360, 0:
+				positionY = positionY + node.amount
+			case 90:
+				positionX = positionX + node.amount
+			case 180:
+				positionY = positionY - node.amount
+			case 270:
+				positionX = positionX - node.amount
+			}
+		case "R":
+			bearing = bearing + node.amount
+			if bearing > 360 {
+				bearing = bearing - 360
+			} else if bearing < 0 {
+				bearing = bearing + 360
+			}
+		case "L":
+			bearing = bearing - node.amount
+			if bearing > 360 {
+				bearing = bearing - 360
+			} else if bearing < 0 {
+				bearing = bearing + 360
+			}
+		}
+	}
+
+	log.Printf("day12 part 1 answer: %d", abs(positionX)+abs(positionY))
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+type node struct {
+	direction string
+	amount    int
+}
+
+func setup() []node {
 	f, err := os.Open("./day-12/input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -28,8 +87,8 @@ func main() {
 			log.Fatal(err)
 		}
 		directions = append(directions, node{
-			Direction: matches[0][1],
-			Amount:    a,
+			direction: matches[0][1],
+			amount:    a,
 		})
 	}
 
@@ -37,58 +96,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	bearing := 90  // Start facing east (090°).
-	positionX := 0 // East, West
-	positionY := 0 // North, South
-	for _, node := range directions {
-		switch node.Direction {
-		case "S":
-			positionY = positionY - node.Amount
-		case "N":
-			positionY = positionY + node.Amount
-		case "E":
-			positionX = positionX + node.Amount
-		case "W":
-			positionX = positionX - node.Amount
-		case "F":
-			switch bearing {
-			case 360, 0:
-				positionY = positionY + node.Amount
-			case 90:
-				positionX = positionX + node.Amount
-			case 180:
-				positionY = positionY - node.Amount
-			case 270:
-				positionX = positionX - node.Amount
-			}
-		case "R":
-			bearing = bearing + node.Amount
-			if bearing > 360 {
-				bearing = bearing - 360
-			} else if bearing < 0 {
-				bearing = bearing + 360
-			}
-		case "L":
-			bearing = bearing - node.Amount
-			if bearing > 360 {
-				bearing = bearing - 360
-			} else if bearing < 0 {
-				bearing = bearing + 360
-			}
-		}
-	}
-
-	log.Printf("day11 part 1 answer: %d", abs(positionX)+abs(positionY))
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-type node struct {
-	Direction string
-	Amount    int
+	return directions
 }
